177473d4b76e9065f65dda302abd12dd1ea5899a
antbenar <antbenar1999@gmail.com>
2017-03-27 22:35:28 -0500

    Update convetir_num_to_letras.cpp

--- convetir_num_to_letras.cpp
+++ convetir_num_to_letras.cpp
@@ -12,35 +12,45 @@ private:
     string p1,p2,p3;
     long long num;
 
-    void decenas(int posPalabras, int temp){//usado para dar nombre a los dos primeros numeros
-        p2=palabras[posPalabras];
-        if(temp!=0){
+    void decenas(int decena, int temp,bool val=true){//usado para dar nombre a los dos primeros numeros
+        p2=palabras[decena];
+        if(temp!=0&&(!val||num%10!=1)){//val se usa como validador para evitar el uno mil
             p2+="y ";
             p2+=palabras[temp];
         }
+        else{
+            p2+="y ";
+            p2+=palabras[22];
+        }
     }
 
-    void unid_y_dec(int num1){//da nombre a los dos primeros numeros
+    void unid_y_dec(int num1,bool val=true){//da nombre a los dos primeros numeros
         if(num1<=20)
             p2=palabras[num1];
         else if(num1<30){
-            p2=palabras[21];
-            p2+=palabras[num1%10];
+            if(!val||num%10!=1){
+                p2=palabras[21];
+                p2+=palabras[num1%10];
+            }
+            else{
+                p2=palabras[21];
+                p2+=palabras[22];
+            }
         }
-        else if(num1<40)decenas(23, num1%10);
-        else if(num1<50)decenas(24, num1%10);
-        else if(num1<60)decenas(25, num1%10);
-        else if(num1<70)decenas(26, num1%10);
-        else if(num1<80)decenas(27, num1%10);
-        else if(num1<90)decenas(28, num1%10);
-        else if(num1<100)decenas(29, num1%10);
+        else if(num1<40)decenas(23, num1%10,val);
+        else if(num1<50)decenas(24, num1%10,val);
+        else if(num1<60)decenas(25, num1%10,val);
+        else if(num1<70)decenas(26, num1%10,val);
+        else if(num1<80)decenas(27, num1%10,val);
+        else if(num1<90)decenas(28, num1%10,val);
+        else if(num1<100)decenas(29, num1%10,val);
     }
 
-    void centenas(int num1){
+    void centenas(int num1,bool val=true){
         if(num1==100)
             p2=palabras[30];
         else{
-            unid_y_dec(num1%100);
+            unid_y_dec(num1%100,val);
             if(num1/100==1)p2.insert(0,palabras[31]);
             else if(num1/100==2)p2.insert(0,palabras[32]);
             else if(num1/100==3)p2.insert(0,palabras[33]);
@@ -53,11 +63,11 @@ private:
         }
     }
 
-    void millar(long long num1){
-        centenas(num1%1000);
+    void millar(long long num1,bool val=true){
+        centenas(num1%1000,val);
         p3=p2;
         if(num1<2000&&num1>=1000)
-            p3.insert(0,palabras[40]);//Evitar el un mil
+            p3.insert(0,palabras[40]);//Evitar el un mil, sino poner solo mil
         else if(num1>=2000){
             centenas(num1/1000);
             p3.insert(0,p2);//Estas son las tres cifras que estan antes del mil: 999 000
@@ -70,7 +80,7 @@ private:
             p1=palabras[43];
             return;
         }
-        millar(num%1000000);
+        millar(num%1000000,false);
         p1=p3;
         if(num<2000000 && num>=1000000){
             p1.insert(0,palabras[41]);//"un millon"
@@ -86,7 +96,7 @@ public:
 
     LeerNumero(long long num_){
         palabras.push_back("");
-        palabras.push_back("uno");
+        palabras.push_back("uno ");
         palabras.push_back("dos ");
         palabras.push_back("tres ");
         palabras.push_back("cuatro ");
@@ -95,7 +105,7 @@ public:
         palabras.push_back("siete ");
         palabras.push_back("ocho ");
         palabras.push_back("nueve ");
-        palabras.push_back("diez ");
+        palabras.push_back("diez ");//10
         palabras.push_back("once ");
         palabras.push_back("doce ");
         palabras.push_back("trece ");
@@ -105,9 +115,9 @@ public:
         palabras.push_back("diecisiete ");
         palabras.push_back("dieciocho ");
         palabras.push_back("diecinueve ");
-        palabras.push_back("veinte ");
+        palabras.push_back("veinte ");//20
         palabras.push_back("veinti");
-        palabras.push_back(" ");
+        palabras.push_back("un ");//22
         palabras.push_back("treinta ");
         palabras.push_back("cuarenta ");
         palabras.push_back("cincuenta ");
@@ -115,7 +125,7 @@ public:
         palabras.push_back("setenta ");
         palabras.push_back("ochenta ");
         palabras.push_back("noventa ");
-        palabras.push_back("cien ");
+        palabras.push_back("cien ");//30
         palabras.push_back("ciento ");
         palabras.push_back("doscientos ");
         palabras.push_back("trescientos ");
@@ -125,16 +135,16 @@ public:
         palabras.push_back("setecientos ");
         palabras.push_back("ochocientos ");
         palabras.push_back("novecientos ");
-        palabras.push_back("mil ");
+        palabras.push_back("mil ");//40
         palabras.push_back("un millon ");
         palabras.push_back("millones ");
         palabras.push_back("cero");
 
         num=num_;
-        if(num>=0 && num<=999999999999)
+        if(num>0 && num<=999999999999)
             millon();
         else
-            cout<<"ingrese un numero mayor igual a 0 y menor a 999 999 999 999"<< endl;
+            cout<<"ingrese un numero mayor a 0 y menor a 999 999 999 999"<< endl;
     }
 
     void imprimir(){
@@ -144,7 +154,7 @@ public:
 
 int main()
 {
-    LeerNumero num(0);
+    LeerNumero num(999999999999);//999 999 999 999
     num.imprimir();
     return 0;
 }
